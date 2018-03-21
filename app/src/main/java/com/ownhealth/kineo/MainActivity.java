4bfb1064479eebb37921d9945cf38d1148b76b97
Agustin Madina <agustinmadina@gmail.com>
2018-03-21 17:35:46 -0300

    Angle between 270 and 360 missing, need to check clockwise, now being set only after 90° is passed, if you go other side it will not function correctly

--- app/src/main/java/com/ownhealth/kineo/MainActivity.java
+++ app/src/main/java/com/ownhealth/kineo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ownhealth.kineo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,7 +10,6 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,69 +22,78 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.view.View.VISIBLE;
 import static java.lang.Math.abs;
+import static java.lang.Math.pow;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
 
     SensorManager sensorManager;
     float[] gData = new float[3];
-    private TextView xTextView;
-    private TextView yTextView;
-    private TextView zTextView;
-    private int yInitialDegree = 0;
-    private int yActualDegree;
-    float yaw;
-    float pitch;
-    float pitchActual;
-    float pitchInitial;
-    float roll;
-    int x;
-    int y;
-    int z;
+    private Toolbar toolbar;
+    private TextView yActualTextView;
+    private TextView yMeasuredActualTextView;
+    private TextView yMeasuredFinalTextView;
+    private float pitch;
+    private float pitchInitial;
+    private int y;
+    private int yInitialDegree = 1;
+    private int measuredAngle;
+    private boolean isMeasuring = false;
+    private boolean clockwise;
+    private boolean clockwiseIsSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        toolbar = setUpToolbar();
+        yActualTextView = findViewById(R.id.y_actual);
+        yMeasuredActualTextView = findViewById(R.id.y_measured_actual);
+        yMeasuredFinalTextView = findViewById(R.id.y_measured_final);
+        final Button stopMeasuringButton = findViewById(R.id.btn_stop_measuring);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Set" + yActualDegree + "as initial degree (0)", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Set" + y + "as initial degree (0)", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                yInitialDegree = yActualDegree;
-                pitchInitial = pitchActual;
-                yTextView.setText("0");
+                yInitialDegree = y;
+                pitchInitial = pitch;
+                yMeasuredActualTextView.setText("0");
+                yMeasuredActualTextView.setVisibility(VISIBLE);
+                stopMeasuringButton.setVisibility(VISIBLE);
+                isMeasuring = true;
+                clockwiseIsSet = false;
             }
         });
-        Button stopMeasuringButton = findViewById(R.id.btn_stop_measuring);
         stopMeasuringButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                zTextView.setText(roundUp((int) Math.round(Math.toDegrees(Math.atan((double) pitch / (double) roll))););
+                yMeasuredFinalTextView.setText(measuredAngle);
             }
         });
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         // Get the sensor manager from system services
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        xTextView = findViewById(R.id.x_container);
-        yTextView = findViewById(R.id.y_container);
-        zTextView = findViewById(R.id.z_container);
+    }
+
+    private Toolbar setUpToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -133,7 +143,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -146,6 +156,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         sensorManager.registerListener(this, asensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onSensorChanged(SensorEvent event) {
         switch (event.sensor.getType()) {
@@ -153,34 +164,52 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 gData[0] = event.values[0];
                 gData[1] = event.values[1];
                 gData[2] = event.values[2];
-                yaw = (float) (gData[0] / 9.82);
+                float yaw = (float) (gData[0] / 9.82);
                 pitch = (float) (gData[1] / 9.82);
-                roll = (float) (gData[2] / 9.82);
-
-                x = (int) Math.round(Math.toDegrees(Math.atan((double) yaw / (double) pitch)));
+                float roll = (float) (gData[2] / 9.82);
+                int x = (int) Math.round(Math.toDegrees(Math.atan((double) yaw / (double) pitch)));
                 y = (int) Math.round(Math.toDegrees(Math.atan((double) pitch / (double) roll)));
-                z = (int) Math.round(Math.toDegrees(Math.atan((double) roll / (double) yaw)));
-
-                yActualDegree = y;
-                pitchActual = pitch;
-                xTextView.setText("y: " + String.valueOf((y)));
-                if (Math.signum(y) == Math.signum(yInitialDegree) && (Math.signum(pitchActual) == (Math.signum(pitchInitial)))) {
-                    //Less than 90° turn
-                    yTextView.setText("y: " + String.valueOf((abs(y - yInitialDegree))));
-                } else if (Math.signum(pitchActual) == (Math.signum(pitchInitial))) {
-                    //Between 90° and 180° turn, Quarter next to it, both up or down
-                    yTextView.setText("y: " + String.valueOf((180 - abs(y) - abs(yInitialDegree))));
-                } else if (Math.signum(y) == Math.signum(yInitialDegree)) {
-                    if (Math.signum(pitch) == 1) {
-                        //Between 180° and 270° turn, ends up
-                        yTextView.setText("y: " + String.valueOf((180 - abs(y) + abs(yInitialDegree))));
-                    } else {
-                        //Between 180° and 270° turn, ends down
-                        yTextView.setText("y: " + String.valueOf((180 + abs(y) - abs(yInitialDegree))));
+                int z = (int) Math.round(Math.toDegrees(Math.atan((double) roll / (double) yaw)));
+                yActualTextView.setText(String.valueOf((y)));
+                if (isMeasuring) {
+                    if (!clockwiseIsSet && (Math.signum(y) != Math.signum(yInitialDegree))) {
+                        //cambio cuadrante y chequeo para que lado segun donde este
+                        if (((Math.signum(yInitialDegree) == -1) && (Math.signum(pitch) == Math.signum(pitchInitial))) || ((Math.signum(yInitialDegree) == 1) && (Math.signum(pitch) != Math.signum(pitchInitial)))) {
+                            clockwise = true;
+                            clockwiseIsSet = true;
+                        } else {
+                            clockwise = false;
+                            clockwiseIsSet = true;
+                        }
                     }
+                }
+
+                if (Math.signum(y) == Math.signum(yInitialDegree) && (Math.signum(pitch) == (Math.signum(pitchInitial)))) {
+                    //Less than 90° turn
+                    measuredAngle = abs(y - yInitialDegree);
+                    yMeasuredActualTextView.setText(String.valueOf(measuredAngle));
+                } else if (Math.signum(y) != Math.signum(yInitialDegree) && (Math.signum(pitch) == (Math.signum(pitchInitial)) && measuredAngle < 270)) {
+                    //Between 90° and 180° turn, Quarter next to it, both up or down
+                    measuredAngle = 180 - abs(y) - abs(yInitialDegree);
+                    yMeasuredActualTextView.setText(String.valueOf(measuredAngle));
+                } else if (Math.signum(y) == Math.signum(yInitialDegree)) {
+                    if ((Math.signum(y) == 1 && clockwise) || (Math.signum(y) == -1 && !clockwise)) { //TODO
+                        //Between 180° and 270° turn
+                        measuredAngle = 180 - abs(y) + abs(yInitialDegree);
+                        yMeasuredActualTextView.setText(String.valueOf(measuredAngle));
+                    } else {
+                        //Between 180° and 270° turn
+                        measuredAngle = 180 + abs(y) - abs(yInitialDegree);
+                        yMeasuredActualTextView.setText(String.valueOf(measuredAngle));
+                    }
+                } else if (Math.signum(pitch) != (Math.signum(pitchInitial))) {
+                    //Between 90° and 180° turn, both left or right //TODO
+                    measuredAngle = abs(yInitialDegree) + abs(y);
+                    yMeasuredActualTextView.setText(String.valueOf(measuredAngle));
                 } else {
-                    //Between 90° and 180° turn, both left or right
-                    yTextView.setText("y: " + String.valueOf((abs(yInitialDegree) + abs(y))));
+                    //Between 270° and 360° turn
+                    measuredAngle = 360 - abs(y) - abs(yInitialDegree);
+                    yMeasuredActualTextView.setText(measuredAngle);
                 }
         }
     }
