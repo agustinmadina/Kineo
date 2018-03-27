7ad1d0569b476193934226b7bf6282568b721577
Agustin Madina <agustinmadina@gmail.com>
2018-03-27 17:32:39 -0300

    X measurement logic added

--- app/src/main/java/com/ownhealth/kineo/activities/MainActivity.java
+++ app/src/main/java/com/ownhealth/kineo/activities/MainActivity.java
@@ -20,11 +20,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ownhealth.kineo.R;
 
 import java.util.ArrayList;
 
+import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 import static java.lang.Math.abs;
 import static java.lang.String.format;
@@ -35,17 +37,30 @@ import static java.lang.String.valueOf;
  */
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
 
-    SensorManager sensorManager;
+    private static final String Y_AXIS = "y_axis";
+    private static final String X_AXIS = "x_axis";
+
+    private SensorManager sensorManager;
     float[] gData = new float[3];
     private TextView yActualTextView;
-    private TextView yMeasuredActualTextView;
-    private TextView yMeasuredFinalTextView;
+    private TextView xActualTextView;
+    private TextView zActualTextView;
+    private TextView actualDegreeTextView;
+    private TextView finalDegreeTextView;
+    private FloatingActionButton fabStartStop;
+    private FloatingActionButton fabChangeAxis;
+    private float yaw;
     private float pitch;
     private int y;
-    private float pitchInitial;
-    private int yInitialDegree = 1;
+    private int x;
+    private int z;
+    private int axisMeasured;
+    private float referenceAxis;
+    private String axisBeingMeasured = Y_AXIS;
+    private float referenceInitial;
+    private int initialDegree = 1;
     private int lastQuarterDegree;
-    private float lastQuarterPitch;
+    private float lastQuarterReference;
     private int measuredAngle;
     private int lastFivePointer = 0;
     private boolean isMeasuring = false;
@@ -59,47 +74,63 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         setUpToolbarAndDrawer();
         yActualTextView = findViewById(R.id.y_actual);
-        yMeasuredActualTextView = findViewById(R.id.y_measured_actual);
-        yMeasuredFinalTextView = findViewById(R.id.y_measured_final);
-        final FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        xActualTextView = findViewById(R.id.x_actual);
+        zActualTextView = findViewById(R.id.z_actual);
+        actualDegreeTextView = findViewById(R.id.measured_actual);
+        finalDegreeTextView = findViewById(R.id.measured_final);
+        fabStartStop = findViewById(R.id.fab_start_stop);
+        fabStartStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isMeasuring) {
-                    finishMeasureClick(fab);
+                    finishMeasureClick();
                 } else {
-                    startMeasureClick(view, fab);
+                    startMeasureClick(view);
                 }
                 isMeasuring = !isMeasuring;
+            }
+        });
+        fabChangeAxis = findViewById(R.id.fab_change_axis);
+        fabChangeAxis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (axisBeingMeasured.equals(Y_AXIS)) {
+                    axisBeingMeasured = X_AXIS;
+                } else {
+                    axisBeingMeasured = Y_AXIS;
+                }
+                Toast.makeText(getApplicationContext(), String.format(getString(R.string.change_axis_being_measured), axisBeingMeasured), Toast.LENGTH_LONG).show();
             }
         });
         // Get the sensor manager from system services
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     }
 
-    private void startMeasureClick(View view, FloatingActionButton fab) {
-        yInitialDegree = y;
-        lastQuarterDegree = y;
-        pitchInitial = pitch;
-        lastQuarterPitch = pitch;
-        yMeasuredActualTextView.setText("0°");
-        yMeasuredActualTextView.setVisibility(VISIBLE);
-        yMeasuredFinalTextView.setVisibility(View.INVISIBLE);
-        fab.setImageResource(R.drawable.ic_media_pause);
-        Snackbar.make(view, "Set " + y + " as initial degree", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    private void startMeasureClick(View view) {
+        initialDegree = axisMeasured;
+        lastQuarterDegree = axisMeasured;
+        referenceInitial = referenceAxis;
+        lastQuarterReference = referenceAxis;
+        actualDegreeTextView.setText("0°");
+        actualDegreeTextView.setVisibility(VISIBLE);
+        finalDegreeTextView.setVisibility(View.INVISIBLE);
+        fabStartStop.setImageResource(R.drawable.ic_media_pause);
+        fabChangeAxis.setVisibility(GONE);
+        Snackbar.make(view, "Set " + axisMeasured + " as initial degree", Snackbar.LENGTH_LONG).setAction("Action", null).show();
     }
 
-    private void finishMeasureClick(FloatingActionButton fab) {
+    private void finishMeasureClick() {
         fillLastFive(measuredAngle);
-        yMeasuredFinalTextView.setVisibility(VISIBLE);
-        yMeasuredFinalTextView.setText(format(getResources().getString(R.string.final_degree_measured), measuredAngle));
-        yMeasuredActualTextView.setText(getString(R.string.ready_to_measure));
-        pitchInitial = 0;
-        yInitialDegree = 1;
+        finalDegreeTextView.setVisibility(VISIBLE);
+        finalDegreeTextView.setText(format(getResources().getString(R.string.final_degree_measured), measuredAngle));
+        actualDegreeTextView.setText(getString(R.string.ready_to_measure));
+        referenceInitial = 0;
+        initialDegree = 1;
         lastQuarterDegree = 0;
-        lastQuarterPitch = 0;
+        lastQuarterReference = 0;
         measuredAngle = 0;
-        fab.setImageResource(R.drawable.ic_media_play);
+        fabStartStop.setImageResource(R.drawable.ic_media_play);
+        fabChangeAxis.setVisibility(VISIBLE);
     }
 
     private void fillLastFive(int measuredAngle) {
@@ -112,10 +143,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         lastFiveLayout.setVisibility(VISIBLE);
         lastFiveList.add(measuredAngle);
         lastFiveLastTextView.setText(format(getString(R.string.last_5_1), lastFiveList.get(lastFivePointer)));
-        lastFive2TextView.setText(lastFiveList.size() >= 2 ? format(getString(R.string.last_5_2), lastFiveList.get(lastFivePointer -1)) : "");
-        lastFive3TextView.setText(lastFiveList.size() >= 3 ? format(getString(R.string.last_5_3), lastFiveList.get(lastFivePointer -2)) : "");
-        lastFive4TextView.setText(lastFiveList.size() >= 4 ? format(getString(R.string.last_5_4), lastFiveList.get(lastFivePointer -3)) : "");
-        lastFive5TextView.setText(lastFiveList.size() >= 5 ? format(getString(R.string.last_5_5), lastFiveList.get(lastFivePointer -4)) : "");
+        lastFive2TextView.setText(lastFiveList.size() >= 2 ? format(getString(R.string.last_5_2), lastFiveList.get(lastFivePointer - 1)) : "");
+        lastFive3TextView.setText(lastFiveList.size() >= 3 ? format(getString(R.string.last_5_3), lastFiveList.get(lastFivePointer - 2)) : "");
+        lastFive4TextView.setText(lastFiveList.size() >= 4 ? format(getString(R.string.last_5_4), lastFiveList.get(lastFivePointer - 3)) : "");
+        lastFive5TextView.setText(lastFiveList.size() >= 5 ? format(getString(R.string.last_5_5), lastFiveList.get(lastFivePointer - 4)) : "");
         lastFivePointer++;
     }
 
@@ -204,59 +235,70 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 gData[0] = event.values[0];
                 gData[1] = event.values[1];
                 gData[2] = event.values[2];
-                float yaw = (float) (gData[0] / 9.82);
+                yaw = (float) (gData[0] / 9.82);
                 pitch = (float) (gData[1] / 9.82);
                 float roll = (float) (gData[2] / 9.82);
-                int x = (int) Math.round(Math.toDegrees(Math.atan((double) yaw / (double) pitch)));
+                x = (int) Math.round(Math.toDegrees(Math.atan((double) yaw / (double) pitch)));
                 y = (int) Math.round(Math.toDegrees(Math.atan((double) pitch / (double) roll)));
-                int z = (int) Math.round(Math.toDegrees(Math.atan((double) roll / (double) yaw)));
-                yActualTextView.setText(valueOf((y)));
+                z = (int) Math.round(Math.toDegrees(Math.atan((double) roll / (double) yaw)));
 
-                if (isMeasuring && y != 0 && abs(y) != 90) {
+                yActualTextView.setText(valueOf((y)));
+                xActualTextView.setText(valueOf((x)));
+                zActualTextView.setText(valueOf((z)));
+
+                if (axisBeingMeasured.equals(Y_AXIS)) {
+                    axisMeasured = y;
+                    referenceAxis = pitch;
+                } else {
+                    axisMeasured = x;
+                    referenceAxis = yaw;
+                }
+
+                if (isMeasuring && axisMeasured != 0 && abs(axisMeasured) != 90) {
                     evaluateIfClockwise();
                     //problema si initial 0, no entra en este al no tener signo
-                    if ((Math.signum(y) == Math.signum(yInitialDegree) && (Math.signum(pitch) == (Math.signum(pitchInitial)))) && measuredAngle < 90) {
+                    if ((Math.signum(axisMeasured) == Math.signum(initialDegree) && (Math.signum(referenceAxis) == (Math.signum(referenceInitial)))) && measuredAngle < 90) {
                         //Less than 90° turn
-                        measuredAngle = abs(y - yInitialDegree);
+                        measuredAngle = abs(axisMeasured - initialDegree);
                         isClockwiseSet = false;
-                    } else if (Math.signum(y) == Math.signum(yInitialDegree) && (Math.signum(pitch) == (Math.signum(pitchInitial)))) {
+                    } else if (Math.signum(axisMeasured) == Math.signum(initialDegree) && (Math.signum(referenceAxis) == (Math.signum(referenceInitial)))) {
                         //Has already done an entire turn as is on the same quarter as the initial degree
-                        yMeasuredActualTextView.setText("Maximum reached");
+                        actualDegreeTextView.setText("Maximum reached");
                         break;
-                    } else if (Math.signum(y) != Math.signum(yInitialDegree) && (Math.signum(pitch) == (Math.signum(pitchInitial)) && measuredAngle < 180)) {
+                    } else if (Math.signum(axisMeasured) != Math.signum(initialDegree) && (Math.signum(referenceAxis) == (Math.signum(referenceInitial)) && measuredAngle < 180)) {
                         //Between 90° and 180° turn, Quarter next to it, both up or down
-                        measuredAngle = 180 - abs(y) - abs(yInitialDegree);
-                    } else if (Math.signum(y) == Math.signum(yInitialDegree)) {
-                        if ((Math.signum(y) == 1 && clockwise) || (Math.signum(y) == -1 && !clockwise)) {
+                        measuredAngle = 180 - abs(axisMeasured) - abs(initialDegree);
+                    } else if (Math.signum(axisMeasured) == Math.signum(initialDegree)) {
+                        if ((Math.signum(axisMeasured) == 1 && clockwise) || (Math.signum(axisMeasured) == -1 && !clockwise)) {
                             //Between 180° and 270° turn
-                            measuredAngle = 180 - abs(y) + abs(yInitialDegree);
+                            measuredAngle = 180 - abs(axisMeasured) + abs(initialDegree);
                         } else {
                             //Between 180° and 270° turn
-                            measuredAngle = 180 + abs(y) - abs(yInitialDegree);
+                            measuredAngle = 180 + abs(axisMeasured) - abs(initialDegree);
                         }
-                    } else if ((Math.signum(pitch) != (Math.signum(pitchInitial)) && measuredAngle < 180)) {
+                    } else if ((Math.signum(referenceAxis) != (Math.signum(referenceInitial)) && measuredAngle < 180)) {
                         //Between 90° and 180° turn, both left or right
-                        measuredAngle = abs(yInitialDegree) + abs(y);
-                    } else if ((Math.signum(pitch) == Math.signum(pitchInitial)) && measuredAngle < 270) {
+                        measuredAngle = abs(initialDegree) + abs(axisMeasured);
+                    } else if ((Math.signum(referenceAxis) == Math.signum(referenceInitial)) && measuredAngle < 270) {
                         //Between 270° and 360° turn, up or down
-                        measuredAngle = 180 + abs(y) + abs(yInitialDegree);
+                        measuredAngle = 180 + abs(axisMeasured) + abs(initialDegree);
                     } else {
                         //Between 270° and 360° turn, right or left
-                        measuredAngle = 360 - abs(y) - abs(yInitialDegree);
+                        measuredAngle = 360 - abs(axisMeasured) - abs(initialDegree);
                     }
-                    yMeasuredActualTextView.setText(format(getResources().getString(R.string.actual_degree_measuring), measuredAngle));
+                    actualDegreeTextView.setText(format(getResources().getString(R.string.actual_degree_measuring), measuredAngle));
                 }
         }
     }
 
     private void evaluateIfClockwise() {
-        if (Math.signum(y) != Math.signum(lastQuarterDegree) && measuredAngle < 90) {
+        if (Math.signum(axisMeasured) != Math.signum(lastQuarterDegree) && measuredAngle < 90) {
             //cambio cuadrante y chequeo para que lado segun los signos de Y y pitch anteriores y actuales, solo setea en primero cuadrante
             if (!isClockwiseSet) {
-                clockwise = ((Math.signum(lastQuarterDegree) == -1) && (Math.signum(pitch) == Math.signum(lastQuarterPitch))) || ((Math.signum(lastQuarterDegree) == 1) && (Math.signum(pitch) != Math.signum(lastQuarterPitch)));
+                clockwise = ((Math.signum(lastQuarterDegree) == -1) && (Math.signum(referenceAxis) == Math.signum(lastQuarterReference))) || ((Math.signum(lastQuarterDegree) == 1) && (Math.signum(referenceAxis) != Math.signum(lastQuarterReference)));
                 isClockwiseSet = true;
-                lastQuarterDegree = y;
-                lastQuarterPitch = pitch;
+                lastQuarterDegree = axisMeasured;
+                lastQuarterReference = referenceAxis;
             }
         }
     }
